004f15997b7304dbe74548eb1eddf5f561a8b885
feliujosefina <mjosefinafe@gmail.com>
2023-02-09 17:01:53 -0300

    sigo creando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BannerComponent } from './banner/banner.component';
+import { FotoPerfilComponent } from './foto-perfil/foto-perfil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    BannerComponent
+    BannerComponent,
+    FotoPerfilComponent
   ],
   imports: [
     BrowserModule,
